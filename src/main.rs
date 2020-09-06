9a7371d3bfc475aa01b84679e3c6c59f5b8fabfb
David Tolnay <dtolnay@gmail.com>
2020-09-06 13:06:50 -0700

    Get cargo metadata

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::env;
+use std::ffi::OsString;
+use std::process::Command;
+
+fn main() -> Result<()> {
+    let cargo = env::var_os("CARGO").unwrap_or(OsString::from("cargo"));
+    let metadata = Command::new(cargo)
+        .arg("metadata")
+        .arg("--format-version")
+        .arg("1")
+        .output()?;
+
+    Ok(())
 }
