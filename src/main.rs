28be2e30902927cfca1e37f8bb7ed1206ef86e7c
David Tolnay <dtolnay@gmail.com>
2020-09-06 13:10:57 -0700

    Rm the workspace lockfile

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::{Context, Result};
 use serde::Deserialize;
 use std::env;
 use std::ffi::OsString;
+use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 
@@ -19,6 +20,10 @@ fn main() -> Result<()> {
         .output()?;
     let metadata: Metadata = serde_json::from_slice(&output.stdout)
         .context("Failed to parse project metadata from Cargo")?;
-
+    let workspace_lockfile = metadata.workspace_root.join("Cargo.lock");
+    if workspace_lockfile.exists() {
+        fs::remove_file(&workspace_lockfile)
+            .with_context(|| format!("Failed to remove {}", workspace_lockfile.display()))?;
+    }
     Ok(())
 }
