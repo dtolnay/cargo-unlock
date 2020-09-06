7d692728b592150dcf1e2e1ce79c08eb09bb73a8
David Tolnay <dtolnay@gmail.com>
2020-09-06 13:08:55 -0700

    Parse project metadata

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use serde::Deserialize;
 use std::env;
 use std::ffi::OsString;
+use std::path::PathBuf;
 use std::process::Command;
+
+#[derive(Deserialize)]
+struct Metadata {
+    workspace_root: PathBuf,
+}
 
 fn main() -> Result<()> {
     let cargo = env::var_os("CARGO").unwrap_or(OsString::from("cargo"));
-    let metadata = Command::new(cargo)
+    let output = Command::new(cargo)
         .arg("metadata")
         .arg("--format-version")
         .arg("1")
         .output()?;
+    let metadata: Metadata = serde_json::from_slice(&output.stdout)
+        .context("Failed to parse project metadata from Cargo")?;
 
     Ok(())
 }
